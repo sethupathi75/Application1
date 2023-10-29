8ae0c95054f7b62afb196765ceaa717e85c47adc
sethupathi75 <83010699+sethupathi75@users.noreply.github.com>
2023-10-29 10:02:55 +0000

    NA

--- Frontend/dblog/src/App.js
+++ Frontend/dblog/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <h1>sethu</h1>
+      <Navbar/>
     </div>
   );
 }
--- Frontend/dblog/src/Navbar.js
+++ Frontend/dblog/src/Navbar.js
@@ -1,11 +1,34 @@
+import { useState } from 'react'
+
 const Navbar = () => {
+
+    const[add,setAdd]=useState(0)
+    const [blogs,setBlog]=useState([
+        {'Name':'Sethu','Age':20},
+        {'Name':'Raja','Age':30},
+        {'Name':'Mani','Age':18}
+    ])
     const handleClick =(name,e) =>{
-        console.log('hello' + name , e.innerHTML)
+       setAdd(add+10)
+       blogs.map((blog)=>{
+        if (blog.Name=='Sethu'){
+            setBlog[(blog.Age=blog.Age+10)]
+
+        }
+        
+       })
     }
     return ( 
         <dev>
             <dev>Class</dev>
-        <button onClick={(e)=>handleClick('sethu',e)}>click me</button>
+        <button onClick={(e)=>handleClick('sethu',e)}>click me {add} times</button>
+        {blogs.map((blog) => (
+            <div>
+                <h2>{blog.Name}</h2>
+                <p>{blog.Age}</p>
+
+            </div>
+    ))}
         </dev>
         
 
